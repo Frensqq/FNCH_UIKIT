ac774e04dd17eda426c1614900e08911b78c262b
Frensqq <liadovn@yandex.ru>
2026-09-03 23:06:00 +0300

    create Spacers

---
+++ UiKit/src/main/java/com/example/uikit/UI/Spacers.kt
@@ -0,0 +1,25 @@
+package com.example.uikit.UI
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun SpacerH(height: Dp){
+    Spacer(
+        modifier = Modifier
+            .height(height)
+    )
+}
+
+@Composable
+fun SpacerW(width: Dp){
+    Spacer(
+        modifier = Modifier
+            .width(width)
+    )
+}
+
