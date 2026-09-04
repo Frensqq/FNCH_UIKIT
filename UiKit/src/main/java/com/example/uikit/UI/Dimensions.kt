9fd19d2afa25015d203b3151c58e3ad809c5be01
Frensqq <liadovn@yandex.ru>
2026-09-04 22:56:06 +0300

    create primary button

---
+++ UiKit/src/main/java/com/example/uikit/UI/Dimensions.kt
@@ -0,0 +1,23 @@
+package com.example.uikit.UI
+
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+
+
+
+    //Padding
+    val MediumPadding = 14.dp
+
+    //Button
+    val HeightButton = 48.dp
+
+
+    //Rounded
+    val SmallRounded = 8.dp
+    val MediumRounded = 10.dp
+    val LargeRounded = 12.dp
+
+    val MediumAlpha = 0.5f
+    val LargeAlpha = 1f
+}
